e2694dab110fe8feeb0a92868acad14ba05bcbd4
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2022-11-12 15:32:02 -0600

    added quit button

--- PythonApplication1/gui.py
+++ PythonApplication1/gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 from tkinter import ttk
 
-
+import DATA
 
 def printtoScreen(args=[]):
 	print(args)
@@ -34,79 +34,11 @@ L3.pack()
 
 B = ttk.Button(top, text ="Confirm", command=lambda: print(E1.get(1.0, "end-1c"),E2.get(1.0, "end-1c")) )
 B.pack( ipadx=10, ipady = 5)
+B.place(relx=.44, rely=.3, anchor="center")
 
-
+QuitButton = ttk.Button(top, text ="Quit", command=top.destroy)
+QuitButton.pack( ipadx=10, ipady = 5 )
+QuitButton.place(relx=.56, rely=.3, anchor="center")
 
 
 top.mainloop()
-
-
-"""
-import tkinter as tk
-
-
-
-
-def createWindow1():
-     
-
-    def closeWindow():
-       # username = userNameEntry.get()
-        #window.destroy()
-        print(userNameEntry.get())
-
-    #This creates the window
-    window = tk.Tk()
-    #Adds label to window
-    userNameLabel = tk.Label(
-        text="Name:",
-        foreground="orange",
-        background="black",
-        width=50,
-        height=5
-     )
-    passwordLabel = tk.Label(
-        text="Password:",
-        foreground="orange",
-        background="black",
-        width=50,
-        height=5
-     )
-    
-    userNameEntry = tk.Entry(
-        width=30,
-    )
-
-    passwordEntry = tk.Entry(
-        width=30,
-    )
-
-    button = tk.Button(
-        text="Click me!",
-        width=20,
-        height=5,
-        bg="white",
-        fg="black",
-        command = closeWindow()
-        )
-    #This adds the stuff to the window, I think
-    userNameLabel.pack()
-    userNameEntry.pack()
-    passwordLabel.pack()
-    passwordEntry.pack()
-    button.pack()
-    
-
-
-    #This makes the window have to wait until it is closed to close.
-    window.mainloop()
-    print(userNameEntry.get())
-
-
-   # name = entry.get()
-    #print(name)
-
-
-    print("This worked, at least.")
-
-"""
