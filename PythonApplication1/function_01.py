d5a99296d86e90b1f38c2a436685174a1e85d4a7
Rhianna Lowery <RhiannaLowery@outlook.com>
2022-11-12 15:04:32 -0600

    Created clockIn and clockOut functions

--- PythonApplication1/function_01.py
+++ PythonApplication1/function_01.py
@@ -4,8 +4,8 @@ def temp():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
-    start_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M")) #Run when "Clock in" pressed"
-    end_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M")) #Run when "Clock out" is pressed
+     #Run when "Clock in" pressed"
+     #Run when "Clock out" is pressed
 
     print(start_time)
     print("Current Time =", current_time)
@@ -18,28 +18,10 @@ def resetVal():
     return a, b
 
 
-#print(datetime.now().minute())
-"""
-class timer:
-    def __init__(self):
-        self.start = None
-
-
 
 def clockIn(ClockIn):
-    if ClockIn == True:
-        return
-    else:
-        self.start = datetime.now().minute()
-        ClockIn = True
-        return 
+    start_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M"))
 
 
 def clockOut(ClockOut):
-    if ClockOut == True:
-        return
-    else:
-        self.start = None
-        ClockOut = True
-        return 
-   """
+    end_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M"))
