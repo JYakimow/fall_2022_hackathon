c27eb062f6898818dd71867332242b1ce383f2ea
Rhianna Lowery <RhiannaLowery@outlook.com>
2022-11-12 15:12:08 -0600

    Added calculateTimeWorked function

--- PythonApplication1/function_01.py
+++ PythonApplication1/function_01.py
@@ -19,8 +19,16 @@ def resetVal():
 
 
 def clockIn(ClockIn):
+    global start_time
     start_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M"))
+    return start_time
 
 
 def clockOut(ClockOut):
+    global end_time
     end_time = ((int)(now.strftime("%H")) * 60) + (int)(now.strftime("%M"))
+    return end_time
+
+def calculateTimeWorked(start_time, end_time):
+    timeWorked = end_time - start_time
+    return timeWorked
