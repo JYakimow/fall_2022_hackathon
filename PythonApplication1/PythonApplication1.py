6e85ea93ca97faf2c1a5643e777f34ee0633d4e8
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2022-11-12 16:35:42 -0600

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -16,7 +16,7 @@ def main():
 	#gui.createWindow1()
 	#frames_gui.openWindow()
 	frames_gui.openFrame1()
-	#frames_gui.openFrame2()
+	frames_gui.openFrame2()
 	#gui.frame01()
 	#gui.frame02()
 	#print("hello world")
