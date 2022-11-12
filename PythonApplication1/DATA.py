190496f60b375a08e975552114a88999d53f6328
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2022-11-12 14:03:28 -0600

    data system

---
+++ PythonApplication1/DATA.py
@@ -0,0 +1,101 @@
+import json
+import sys
+import os
+
+
+
+def findByID(id):
+    x = 0
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        while x < len(data1["users"]):
+            if(data1["users"][x]["id"] == id):
+                return x
+            x += 1
+
+def getClockedStatus(id):
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        info = findByID(id)
+        return data1["users"][info]["clockedStatus"]
+
+def getTotalMinutes(id):
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        info = findByID(id)
+        return data1["users"][info]["totalMinutes"]
+
+def getPin(id):
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        info = findByID(id)
+        return data1["users"][info]["pin"]
+
+def getName(id):
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        info = findByID(id)
+        return data1["users"][info]['name']
+
+
+def changeClockedStatus(id):
+    info = findByID(id)
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        
+        if(getClockedStatus(id) == True):
+            data1["users"][info]["clockedStatus"] = False
+        else:
+            data1["users"][info]["clockedStatus"] = True
+        f.seek(0)
+        json.dump(data1,f)
+        f.truncate()
+        
+
+    
+def changeHourBreak(id):
+    info = findByID(id)
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        
+        data1["users"][info]["hourBreak"] -= 1
+
+        f.seek(0)
+        json.dump(data1,f)
+        f.truncate()
+        
+
+def changeSmallBreak(id):
+    info = findByID(id)
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        
+        data1["users"][info]["smallBreak"] -= 1
+
+        f.seek(0)
+        json.dump(data1,f)
+        f.truncate()
+    
+        
+def changeMin(id, amount):
+    info = findByID(id)
+    with open(os.path.join(os.path.dirname(sys.argv[0]), 'UserInfo.json'), 'r+') as f:
+        data1 = json.load(f)
+        
+        data1["users"][info]["totalMinutes"] += amount
+
+        f.seek(0)
+        json.dump(data1,f)
+        f.truncate()
+   
+
+def main():
+    print(getClockedStatus(100001))
+    changeClockedStatus(100001)
+    print(getClockedStatus(100001))
+    changeMin(100001,5)
+    
+
+
+if __name__ == "__main__":
+	main()
